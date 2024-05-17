f1482f29630ccf36902e9656897a411829493f6e
GokulKrishna9500 <gokulkrishna.sp@gmail.com>
2024-05-17 15:43:04 +0530

    feat:product search functinality updated

--- products.js
+++ products.js
@@ -9,3 +9,26 @@ menuicon.addEventListener("click", function(){
 closenav.addEventListener("click", function(){
     sidenav.style.right="-50%"
 })
+
+// search functionality
+
+var search = document.getElementById("search")
+var productcontainer = document.getElementById("productContainer")
+var productlist = productcontainer.querySelectorAll("div")
+
+search.addEventListener("keyup",function(){
+    var enteredvalue = event.target.value.toUpperCase()
+    
+    for(count=0;count<productlist.length;count++){
+        
+        var productname = productlist[count].querySelector("h1").textContent
+        
+        if(productname.toUpperCase().indexOf(enteredvalue)<0){
+            productlist[count].style.display="none"
+        }
+        else{
+            productlist[count].style.display="block"
+        }
+    }
+    
+})
